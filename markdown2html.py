38f27732a12538525057e565f1aab1a9150f80cb
Mohammed Amer <loard.amer.2010@gmail.com>
2024-10-21 00:38:40 +0300

    frist push of my scripting parser which parse code written in markdown into html

---
+++ markdown2html.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''this is module takes an mark down written file
+and produce new html file based on it'''
+
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
+        exit(1)
+    try:
+        with open(sys.argv[1], "r") as srcFile:
+            content = srcFile.readlines()
+            with open(sys.argv[2], "w") as desFile:
+                for line in content:
+                    desFile.write(parser(line))
+    except:
+        print(f"missing {sys.argv[1]}", file=sys.stderr)
+
+
+def parser(line):
+    pass
