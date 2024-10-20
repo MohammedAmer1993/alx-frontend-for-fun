4125bb6dafbc9723d933650e8baab64950ffcc4f
Mohammed Amer <loard.amer.2010@gmail.com>
2024-10-21 00:45:35 +0300

    frist push of my scripting parser which parse code written in markdown into html

--- markdown2html.py
+++ markdown2html.py
@@ -5,6 +5,14 @@ and produce new html file based on it'''
 import sys
 
 
+def parser(line):
+    '''function that do the parsing
+    args:
+        line (str): a string ended with newline character to be parsed
+    '''
+    pass
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html",
@@ -15,15 +23,7 @@ if __name__ == "__main__":
             content = srcFile.readlines()
             with open(sys.argv[2], "w") as desFile:
                 for line in content:
-                    desFile.write(parser(line))
+                    desFile.write("parser(line)")
     except FileNotFoundError as e:
         print(f"Missing {sys.argv[1]}", file=sys.stderr)
         exit(1)
-
-
-def parser(line):
-    '''function that do the parsing
-    args:
-        line (str): a string ended with newline character to be parsed
-    '''
-    pass
