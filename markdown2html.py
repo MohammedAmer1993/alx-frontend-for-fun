bf4209721f8e44961173342d47ac0b99a3064f09
Mohammed Amer <loard.amer.2010@gmail.com>
2024-10-21 04:53:23 +0300

    convert headers, ordered and unordered list from markdown to html

--- markdown2html.py
+++ markdown2html.py
@@ -5,12 +5,74 @@ and produce new html file based on it'''
 import sys
 
 
-def parser(line):
+def parseLine(line):
     '''function that do the parsing
     args:
         line (str): a string ended with newline character to be parsed
     '''
-    pass
+    count = 0
+    if line[0] == '#':
+        for ch in line:
+            if ch == '#':
+                count += 1
+            else:
+                break
+        preTag = f"<h{count}>"
+        postTag = f"</h{count}>"
+        bodyStr = ""
+        for ch in line:
+            if ch == "#":
+                continue
+            if ch == "\n":
+                return preTag + bodyStr + postTag + ch
+            bodyStr += ch
+    else:
+        return line
+    return preTag + bodyStr + postTag
+
+
+def parseList(lines):
+    ulCount = 0
+    olCount = 0
+    newlines = []
+    newlines2 = []
+    for line in lines:
+        if line[0] == '-':
+            if ulCount == 0:
+                newlines.append("<ul>\n")
+            ulCount += 1
+        elif ulCount != 0:
+            newlines.append("</ul>\n")
+            ulCount = 0
+        newlines.append(line)
+
+    for line in newlines:
+        if line[0] == '*' and line[1] == ' ':
+            if olCount == 0:
+                newlines2.append("<ol>\n")
+            olCount += 1
+        elif olCount != 0:
+            newlines2.append("</ol>\n")
+            olCount = 0
+        newlines2.append(line)
+    return newlines2
+
+
+def parseListItems(lines):
+    newLines = []
+    for line in lines:
+        mystr = ""
+        pre = "<li>"
+        post = "</li>"
+        for index, ch in enumerate(line):
+            if ch == "-" or ch == "*" and line[index + 1] == " ":
+                mystr = pre + mystr
+                continue
+            if ch == "\n" and pre in mystr:
+                mystr = mystr + post
+            mystr += ch
+        newLines.append(mystr)
+    return newLines
 
 
 if __name__ == "__main__":
@@ -22,8 +84,13 @@ if __name__ == "__main__":
         with open(sys.argv[1], "r") as srcFile:
             content = srcFile.readlines()
             with open(sys.argv[2], "w") as desFile:
+                contentAfterHeader = []
                 for line in content:
-                    desFile.write("parser(line)")
+                    contentAfterHeader.append(parseLine(line))
+                contentAfterHeader = parseList(contentAfterHeader)
+                contentAfterHeader = parseListItems(contentAfterHeader)
+                for item in contentAfterHeader:
+                    desFile.write(item)
     except FileNotFoundError as e:
         print(f"Missing {sys.argv[1]}", file=sys.stderr)
         exit(1)
